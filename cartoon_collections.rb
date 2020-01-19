21e641541995903e3cf5ea6d69d6f15562e6470b
Aaron Minyard <amnyrrd@gmail.com>
2020-01-19 03:10:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,28 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarf_arr)
+  dwarf_arr.each_with_index { |dwarf, index| puts (index + 1).to_s + ".#{dwarf}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map { |call| call.capitalize() + "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(planeteer_calls)
+  planeteer_calls.map do |call| 
+    if call.length > 4 
+      return true
+    else 
+      return false
+    end
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(ingredients)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  ingredients.each do |ing|
+    if ing.include?(cheese_types)
+      return ing
+    elsif !ing.include?(cheese_types)
+      return nil
+    end
+  end
 end
